4df365cf753fe8db7ed655b109c2162a1db9727d
Sean Khottavong <sean.khottavong@gmail.com>
2018-04-23 05:26:30 -0400

    grid update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const title1 = document.getElementById('title1');
-const title2 = document.getElementById('title2');
-const pic = document.querySelector('#pic');
-
-pic.addEventListener("mouseenter", function() {
-  title2.classList.toggle('transform2');
-  title1.classList.toggle('transform1');
-});
-pic.addEventListener("mouseout", function() {
-  title2.classList.toggle('transform2');
-  title1.classList.toggle('transform1');
-});
+// const title1 = document.getElementById('title1');
+// const title2 = document.getElementById('title2');
+// const pic = document.querySelector('#pic');
+//
+// pic.addEventListener("mouseenter", function() {
+//   title2.classList.toggle('transform2');
+//   title1.classList.toggle('transform1');
+// });
+// pic.addEventListener("mouseout", function() {
+//   title2.classList.toggle('transform2');
+//   title1.classList.toggle('transform1');
+// });
 
 // const projects = document.getElementById('projects');
 // const dropdown = document.getElementById('dropdown');
