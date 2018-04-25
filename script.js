a14de7e6e1e8656965d496b84390910914fccd47
Sean Khottavong <sean.khottavong@gmail.com>
2018-04-25 07:34:15 -0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-// const title1 = document.getElementById('title1');
-// const title2 = document.getElementById('title2');
-// const pic = document.querySelector('#pic');
-//
-// pic.addEventListener("mouseenter", function() {
-//   title2.classList.toggle('transform2');
-//   title1.classList.toggle('transform1');
+const rock = document.getElementById('#rock');
+const paper = document.getElementById('#paper');
+const scissors = document.querySelector('#scissors');
+const icon = document.querySelectorAll('i');
+const pcPlayer = document.querySelector('#pcPlayer');
+
+icon.addEventListener("click", function() {
+  pcPlayer.classList.toggle('pcPlayerAnimate')
+});
+
+// scissors.addEventListener("click", function() {
+//   pcPlayer.style.animation = '';
 // });
 // pic.addEventListener("mouseout", function() {
 //   title2.classList.toggle('transform2');
