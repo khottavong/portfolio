c7c7f4823bebbd02d4c30b4a956be438fde5a673
Sean Khottavong <sean.khottavong@gmail.com>
2018-05-07 07:02:43 -0400

    big update

--- script.js
+++ script.js
@@ -1,46 +1,11 @@
-const contactbtn = document.querySelector('#contactbtn');
-const form = document.querySelector('#form')
+const skill1 = document.querySelector('#skill1');
+const skill2 = document.querySelector('#skill2');
+const skill3 = document.querySelector('#skill3');
+const area = document.querySelector('#specialty');
 
-
-
-
-
-// contactbtn.addEventListener("click", function() {
-//   contactbtn.style.display = 'none';
-//   form.classList.toggle('hide');
-//   form.classList.toggle('heighttransition');
-// });
-
-
-function appendStrings() {
-  let outputHtml = '<ul>'
-  let contactArray = ['Sean Khottavong', '423-488-****', 'call me', 'mail me'];
-  for (let i = 0; i < contactArray.length; i++) {
-    outputHtml += '<li>' + contactArray[i] + '</li>'
-  }
-  outputHtml += '</ul>'
-  $('#contact-info').html(outputHtml);
-}
-
-appendStrings();
-
-
-
-
-// scissors.addEventListener("click", function() {
-//   pcPlayer.style.animation = '';
-// });
-// pic.addEventListener("mouseout", function() {
-//   title2.classList.toggle('transform2');
-//   title1.classList.toggle('transform1');
-// });
-
-// const projects = document.getElementById('projects');
-// const dropdown = document.getElementById('dropdown');
-//
-// projects.addEventListener("mouseenter", function() {
-//   dropdown.classList.toggle('dropdownshow');
-// });
-// projects.addEventListener("mouseout", function() {
-//   dropdown.classList.toggle('dropdownshow');
-// });
+area.addEventListener("mouseenter", function() {
+  console.log('hovered');
+  skill1.classList.add('skill1');
+  skill2.classList.add('skill2');
+  skill3.classList.add('skill3');
+});
