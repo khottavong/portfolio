6f0ada7efabc83ddbb437baf28825eab1db8cd5a
Sean Khottavong <sean.khottavong@gmail.com>
2018-05-08 17:13:20 -0400

    big update

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const skill2 = document.querySelector('#skill2');
 const skill3 = document.querySelector('#skill3');
 const area = document.querySelector('#specialty');
 
-area.addEventListener("mouseenter", function() {
-  console.log('hovered');
+
+
+window.addEventListener('load', function() {
+  console.log('hello');
   skill1.classList.add('skill1');
   skill2.classList.add('skill2');
   skill3.classList.add('skill3');
