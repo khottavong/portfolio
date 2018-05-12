c61830772c8e4a10cdc47358c19aa00133549bf9
Sean Khottavong <sean.khottavong@gmail.com>
2018-05-12 13:41:20 -0400

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const skill1 = document.querySelector('#skill1');
-const skill2 = document.querySelector('#skill2');
-const skill3 = document.querySelector('#skill3');
-const area = document.querySelector('#specialty');
+// const skill1 = document.querySelector('#skill1');
+// const skill2 = document.querySelector('#skill2');
+// const skill3 = document.querySelector('#skill3');
+// const area = document.querySelector('#specialty');
 
 // window.addEventListener('load', function() {
 //   console.log('hello');
